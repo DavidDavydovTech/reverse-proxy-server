cc775899c66bcac38a5e13fe97ceb9a4305547ba
David J. Davydov <davydovjd611@gmail.com>
2020-09-16 09:20:15 -0400

    First major update of the server
    Server expects all websites to be stored in a 'configs' folder in the
    root directory of the app. All files within it are expected to be JSON
    files which contain: A name (UNIQUE), domain, tld, and an array of subdomains (UNIQUE).
    
    There's still a lot to be added like a valitator, support for multiple
    subdomains, etc. But until then this will suffice (...once tested that
    is.)

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# reverse-proxy-server
-My reverse proxy server
+# Reverse-Proxy-Server
 
-./apps
-./nodemodules
+### Introduction
+This is a server/'router' that is supposed to take other git projects on my github, launch them, and then route incoming requests to port 80 to the port that they live on. This server assumes that:
+ * Each server will have a 'export.js' file that exports the server via `module.exports = <server>`.
+ * Each server will only export a single thing withing it being in an object, a instanc of `express`.
+ * Each server will handle routing to its own subdomains. If a sub domain is meant to route to a completely different app it should provide a 'exceptions' object where the keys are the subdomains and the values are (for now).
+ * Each server will take in any ports.
+ * Each server that needs a database connects to the mongodb server on the VPS and doesn't have its own database to run. 
+
+### How does this know what servers exist where?
+Each server is expected to have a `.json` file describing everything this server needs to know about it. For subdomains using a `*` wildcard will route any subdomains not found in a website's 'object' will send traffic to the express app in the `!!!DEFAULT!!!` key. DO NOT TRY TO ASSIGN A SUBDOMAIN TO THAT KEY NAME OR HAVE MORE THAN TWO WILDCARDS. Examples will be put here later.
--- server.js
+++ server.js
@@ -1,118 +1,226 @@
-//For web hosting & proxying
-const express = require('express');
-const http = require("http");
+// .Env
+require('dotenv').config()
+// Proxy
 const bouncy = require('bouncy');
-
-//For finding the files in the website folders
+const portScout = require('port-scout');
+// Files
 const path = require('path');
-const fs = require('fs');
-const { config } = require('process');
+const fs = require('fs').promises;
+// Extra
+const chalk = require('chalk')
+const log = {
+    error: (location, error, errorObj) => {
+        console.log(
+            chalk.underline.bgRed(`[${location.toUpperCase()}] ERROR: `), 
+            chalk.red(`${error}\n\n`), 
+            errorObj
+        );
+    }
+}
 
-//For actually getting our apps
-const appConfig = require('./config');
-const apps = appConfig.apps;
+let serverDirectory = {};
+let serversContainer = {};
+const configsPath = path.join(__dirname, process.env.CONFIGS_PATH); //Using the "folder" key create a file path to the website's build folder.
+fs.readdir(configsPath)
+    .then(( configs ) => {
+        return Promise.all( configs.map( ( config ) => {
+            let currentConfigPath = path.join( configsPath + '/' + config );
 
-//This is the middleware/function used to set up and launch the websites.
-let launchWebsites = () => {
-    console.log(apps)
-    for(let website in apps){ // Itterate through websites in the "apps" object
-        website = apps[website]; // Make the "website" variable refrence the current website in the "apps" object.
-    
-        const websitePath = path.join(__dirname+`/apps/${website.folder}${website.build}`); //Using the "folder" key create a file path to the website's build folder.
-        console.log(websitePath)
-        
-        fs.readdir(websitePath, (err, files) => { //Go to the website's build folder and store the filenames in a "files" array
-            if(err){ //If there's an error trying the access the build folder (permissions, build folder doesn't exist, etc) then console log an error and skip the setup.
-                console.log(`${website.host}: An error occured while trying to access files.`)
-                console.log(err)
-            } else {
-                let isHttps = null; //Assigned to true or false in the if statement below, used to launch the server using different methods depending on if its http or https. 
-    
-                if(website.protocol === "http"){ 
-                    isHttps = false;
-            
-                } else if (website.protocol === "https"){
-                    isHttps = true;
-    
-                } else { // Failsafe for when the protocol for the website is missing.
-                    console.log(`Warning a website ("${website.hosts[0]}") tried to be launched but it had no protocol.`)
-                    next();
-                }
-                
-                // Filter the files in the build folder not to include index.html. 
-                // Since our react websites aren't fragmented any routes that lead to a url on the site (for instance "example.com/about") need to be directed to index.html to be delt with there. 
-                // However if there's a specific file that needs to be loaded or a folder that contains files that needs to be served via the server (for example "example.com/images/[etc]") then it needs to actually refrence that file. 
-                // We take care of this by assuming that the developer is competent and that files/folders in the build folder and routes in the React website all have unique names (and that none of them are called index).
-                // Then later down the line if a user requests a route and its in the files array, we assume they need that file to be served to them and that a route with that name does not exist on the React website. Likewise we assume that if we don't have that file name in the array that its a route they're trying to access on the React website itself.
-                const fileArray = files.filter(file => file !== 'index.html')
-                console.log()
-                website.app.use('/*', (req,res) =>{ //We do our own route filtering so we can just use "/*"
-                    let route = req.params["0"].substring(0, req.params["0"].indexOf('/')); // Get the route from the request.
-    
-                    if(fileArray.includes(route)){ // If the first part of the route is in the files array its either a file or a folder, we try to send them the file in the code below.
-                        
-                        // Set the route path to a variable.
-                        // P.S. We don't have to worry about traversal with "../", this is taken care of automatically.
-                        let tempFilePath = path.join(__dirname+`/apps/${website.folder}/build/${req.params["0"]}`); //Set the file path
-
-                        //Check if file exists. If it does send the file, otherwise send a 404 and console.log an error. 
-                        fs.access(tempFilePath, fs.F_OK, (err) => {
-                            if (err) {
-                                console.error(err)
-                                res.statusCode = 404;
-                                res.end('Not Found');
-
-                            } else {
-                                res.sendFile(tempFilePath);
-
-                            }
-                          
-                        })
-
-                    } else { // Otherwise its a route in the React website.
-                        res.sendFile(path.join(__dirname+`/apps/${website.folder}/build/index.html`));
-
-                    }
-                    
+            return fs.readFile(currentConfigPath, 'utf8')
+                .then((config) => {
+                    return JSON.parse(config);
                 });
-            
-                
-                //Temperary warning on the left since I haven't added the ability to deploy https websites. Otherwise deploys a http website perfectly.
-                website.server = isHttps?console.warn(`Warning a website ("${website.hosts[0]}" tried to be launched as a https website but it https isn't supported yet.`):http.createServer(website.app)
-                website.server.listen(website.port) // Listen on the port in the website object.
-    
-                for(let host of website.hosts){ //For every host in the hosts key add their host as the keyname to the correct protocall and assign the port in that website object to that new key. This acts as a directory for bouncy.
-                    bouncyHosts[isHttps?"https":"http"][host] = website.port;
-                    
+        }))
+        .catch((err) => {
+            log.error(
+                '[MAIN] ERROR: ', 
+                'ERROR OCCURRED WHILE TRYING TO READ WEBSITE CONFIGS \n', 
+                err
+            );
+        })
+    })
+    .then(async (servers) => {
+        servers.forEach(async (server, i) => {
+            let domain = `${server.domain}.${server.tld}`;
+            let port = await portScout.range(49152, 65535);
+            let proccessedSubdomains= [];
+
+            if (!serverDirectory.hasOwnProperty(domain)) {
+                serverDirectory[domain] = {};
+            }
+
+            for ( let i in server.subdomains ) {
+                let subdomain = null;
+
+                if ( server.subdomains[i] === '*' ) {
+                    subdomain = '!!!DEFAULT!!!';
+                } else {
+                    subdomain = server.subdomains[i];
+                }
+
+                if ( serverDirectory[domain].hasOwnProperty[subdomain] ) {
+                    proccessedSubdomains.forEach((e) => {
+                        if ( e !== subdomain ) {
+                            delete serverDirectory[domain][subdomain];
+                        }
+                    });
+
+                    log.error(
+                        'MAIN',
+                        `Tried to attatch a server's port to ${subdomain}, but that subdomain already has a port! Aborting server launch. `,
+                        new Error('NO OVERLAPPING SUBDOMAINS')
+                    );
+   
+                    break;
+                } else {
+                    serverDirectory[domain][subdomain] = port; 
+                    proccessedSubdomains.push(subdomain);
                 }
             }
-    
+
+            let appLocation = null;
+            if ( server.directory ) {
+                appLocation = server.directory;
+            } else {
+                appLocation = path.join(__dirname, process.env.DEPLOYABLES_PATH, server.name)
+            }
+
+            let serverApp = null;
+            try {
+                serverApp = require(appLocation);
+            }
+            catch (err) {
+                proccessedSubdomains.forEach((e) => {
+                    delete serverDirectory[domain][subdomain];
+                });
+                
+                log.error(
+                    'main',
+                    `Tried to get a server's app at path "${appLocation}" but an unknown error occured while trying to load it.`,
+                    err
+                )
+            }
+
+            serversContainer[server.name] = serverApp;
+            serversContainer[server.name].listen(port);
         })
-    }
-}
+    })
 
+// //For web hosting & proxying
+// const express = require('express');
+// const http = require("http");
+// const bouncy = require('bouncy');
 
-let bouncyHosts = {
-    http: {},
-    https: {}
-}
+// //For finding the files in the website folders
+// const path = require('path');
+// const fs = require('fs');
+// const { config } = require('process');
 
-launchWebsites(apps);
+// //For actually getting our apps
+// const appConfig = require('./config');
+// const apps = appConfig.apps;
 
-bouncy((req, res, bounce) => {
-    let date = new Date();
-
-    if(bouncyHosts.https[req.headers.host]){
-        bounce(bouncyHosts.https[req.headers.host])
-    } 
+// //This is the middleware/function used to set up and launch the websites.
+// let launchWebsites = () => {
+//     console.log(apps)
+//     for(let website in apps){ // Itterate through websites in the "apps" object
+//         website = apps[website]; // Make the "website" variable refrence the current website in the "apps" object.
     
-    else if (bouncyHosts.http[req.headers.host]){
-        bounce(bouncyHosts.http[req.headers.host])
-    }
+//         const websitePath = path.join(__dirname+`/apps/${website.folder}${website.build}`); //Using the "folder" key create a file path to the website's build folder.
+//         console.log(websitePath)
+        
+//         fs.readdir(websitePath, (err, files) => { //Go to the website's build folder and store the filenames in a "files" array
+//             if(err){ //If there's an error trying the access the build folder (permissions, build folder doesn't exist, etc) then console log an error and skip the setup.
+//                 console.log(`${website.host}: An error occured while trying to access files.`)
+//                 console.log(err)
+//             } else {
+//                 let isHttps = null; //Assigned to true or false in the if statement below, used to launch the server using different methods depending on if its http or https. 
+    
+//                 if(website.protocol === "http"){ 
+//                     isHttps = false;
+            
+//                 } else if (website.protocol === "https"){
+//                     isHttps = true;
+    
+//                 } else { // Failsafe for when the protocol for the website is missing.
+//                     console.log(`Warning a website ("${website.hosts[0]}") tried to be launched but it had no protocol.`)
+//                     next();
+//                 }
+                
+//                 // Filter the files in the build folder not to include index.html. 
+//                 // Since our react websites aren't fragmented any routes that lead to a url on the site (for instance "example.com/about") need to be directed to index.html to be delt with there. 
+//                 // However if there's a specific file that needs to be loaded or a folder that contains files that needs to be served via the server (for example "example.com/images/[etc]") then it needs to actually refrence that file. 
+//                 // We take care of this by assuming that the developer is competent and that files/folders in the build folder and routes in the React website all have unique names (and that none of them are called index).
+//                 // Then later down the line if a user requests a route and its in the files array, we assume they need that file to be served to them and that a route with that name does not exist on the React website. Likewise we assume that if we don't have that file name in the array that its a route they're trying to access on the React website itself.
+//                 const fileArray = files.filter(file => file !== 'index.html')
+//                 console.log()
+//                 website.app.use('/*', (req,res) =>{ //We do our own route filtering so we can just use "/*"
+//                     let route = req.params["0"].substring(0, req.params["0"].indexOf('/')); // Get the route from the request.
+    
+//                     if(fileArray.includes(route)){ // If the first part of the route is in the files array its either a file or a folder, we try to send them the file in the code below.
+                        
+//                         // Set the route path to a variable.
+//                         // P.S. We don't have to worry about traversal with "../", this is taken care of automatically.
+//                         let tempFilePath = path.join(__dirname+`/apps/${website.folder}/build/${req.params["0"]}`); //Set the file path
 
-    else {
-        console.log(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}] ${req.connection.remoteAddress?req.connection.remoteAddress:"UNDEFINED_IP"}: No matching Domain found! Header was "${req.headers.host}"!`)
-        res.statusCode = 404;
-        res.end('Not Found');
-    }
-}).listen(80)
+//                         //Check if file exists. If it does send the file, otherwise send a 404 and console.log an error. 
+//                         fs.access(tempFilePath, fs.F_OK, (err) => {
+//                             if (err) {
+//                                 console.error(err)
+//                                 res.statusCode = 404;
+//                                 res.end('Not Found');
+
+//                             } else {
+//                                 res.sendFile(tempFilePath);
+
+//                             }
+                          
+//                         })
+
+//                     } else { // Otherwise its a route in the React website.
+//                         res.sendFile(path.join(__dirname+`/apps/${website.folder}/build/index.html`));
+
+//                     }
+                    
+//                 });
+            
+                
+//                 //Temperary warning on the left since I haven't added the ability to deploy https websites. Otherwise deploys a http website perfectly.
+//                 website.server = isHttps?console.warn(`Warning a website ("${website.hosts[0]}" tried to be launched as a https website but it https isn't supported yet.`):http.createServer(website.app)
+//                 website.server.listen(website.port) // Listen on the port in the website object.
+    
+//                 for(let host of website.hosts){ //For every host in the hosts key add their host as the keyname to the correct protocall and assign the port in that website object to that new key. This acts as a directory for bouncy.
+//                     bouncyHosts[isHttps?"https":"http"][host] = website.port;
+                    
+//                 }
+//             }
+    
+//         })
+//     }
+// }
+
+
+// let bouncyHosts = {
+//     http: {},
+//     https: {}
+// }
+
+// launchWebsites(apps);
+
+// bouncy((req, res, bounce) => {
+//     let date = new Date();
+
+//     if(bouncyHosts.https[req.headers.host]){
+//         bounce(bouncyHosts.https[req.headers.host])
+//     } 
+    
+//     else if (bouncyHosts.http[req.headers.host]){
+//         bounce(bouncyHosts.http[req.headers.host])
+//     }
+
+//     else {
+//         console.log(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}] ${req.connection.remoteAddress?req.connection.remoteAddress:"UNDEFINED_IP"}: No matching Domain found! Header was "${req.headers.host}"!`)
+//         res.statusCode = 404;
+//         res.end('Not Found');
+//     }
+// }).listen(80)
