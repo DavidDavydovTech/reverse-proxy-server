69c413c1b1b921de208485608794ce8f8623776a
David J. Davydov <davydovjd611@gmail.com>
2020-09-17 23:08:42 -0400

    Add another modification to the request!

--- server.js
+++ server.js
@@ -137,7 +137,11 @@ fs.readdir(configsPath)
 
                                 request({
                                     method: req.method,
-                                    uri: `http://localhost:${portLink[sub]}${req.originalUrl}`
+                                    baseUrl: `http://localhost:${portLink[sub]}`,
+                                    headers: req.headers,
+                                    qs: req.originalUrl,
+                                    body: req.bodyRaw,
+                                    
                                 })
                                     .pipe(res)
                                     .on('finish', (response) => {
