edc046a130a170b1a75cde2248ab47696452262a
David J. Davydov <davydovjd611@gmail.com>
2020-09-19 12:16:57 -0400

    Add fetchApps method

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const net = require("net");
+const { dirname } = require("path");
 const path = require("path");
 const fs = require("fs").promises;
 
 class Urusai {
   constructor(options = {}) {
     // Set up options if they don't exist.
-    options.ports = options.hasOwnProperty('ports') ? options.ports : [ 80 ];
+    options.ports = options.hasOwnProperty("ports") ? options.ports : [80];
     // Bind all methods.
     this.initalize = this.initalize.bind(this);
     this.initalizeHandlers = this.initalizeHandlers.bind(this);
-    this.forwardRequest = this.forwardRequest.bind(this);
 
     // Vars
 
     // Containers
-    this.handlers = {};
+    this.Handlers = {};
 
     // Initalize the class
     this.initalize(options);
@@ -26,9 +26,25 @@ class Urusai {
         return this.initalizeWebConfigs(options.path);
       })
       .then((websiteData) => {
-        console.log(stuff)
+        console.log(stuff);
       })
-      .catch((err)=>{console.log(err)})
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  initalizeOnClose = () => {
+    process.on('SIGTERM', () => {
+      // Shut down handlers: 
+      for (let handlerPort in this.Handlers) {
+        handler = this.Handlers[handlerPort];
+        handler.close();
+        setTimeout(() => {
+          handler.getConnections()
+          console.log(`Handler on port ${handlerPort} is closed`);
+        }, 1000);
+      }
+    });
   }
 
   initalizeHandlers(ports) {
@@ -36,28 +52,54 @@ class Urusai {
       try {
         ports = sanatizePorts(ports);
         for (let port of ports) {
-          this.handlers[port] = initalizeHandler(port);
-          console.log('Handler now listening on port', port)
+          this.Handlers[port] = initalizeHandler(port);
+          console.log("Handler now listening on port", port);
         }
         resolve();
       } catch (err) {
         reject(err);
       }
-    })
+    });
   }
 
   initalizeWebConfigs(dir) {
-    dir = dir ? dir : process.env.CONFIGS_PATH ? process.env.CONFIGS_PATH : 'configs';
-    dir = path.join(__dirname, dir)
+    dir = dir
+      ? dir
+      : process.env.CONFIGS_PATH
+      ? process.env.CONFIGS_PATH
+      : "configs";
+    dir = path.join(__dirname, dir);
 
     return parseConfigsJSON(dir);
+  }
+
+  fetchApps(configs) {
+    for (let config of configs) {
+      dir = config.dir
+        ? config.dir
+        : process.env.DEPLOYABLES_PATH
+        ? process.env.DEPLOYABLES_PATH
+        : "deployables";
+      dir = path.join(__dirname, dir, config.name);
+
+      if (this.Apps.hasOwnProperty(config.name)) {
+        throw new Error(`APP ${config.name} ALREADY EXISTS (APPS MUST HAVE A UNIQUE "NAME")`)
+      } else {
+        this.Apps[config.name] = require('dir');
+      }
+    }
   }
 }
 
 module.exports = Urusai;
 
+
+
+
+// Helpers
+
 let sanatizePorts = (ports) => {
-  ports = [...ports]
+  ports = [...ports];
   let included = [];
   return ports.filter((e) => {
     if (isNaN(parseInt(e)) === false && included.includes(e) === false) {
@@ -117,4 +159,4 @@ let forwardRequest = ({ url = "localhost", port = "80", request }) => {
       })
       .write(request);
   });
-}
+};
